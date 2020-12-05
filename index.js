578e7704a927bc622128008a7471b60fc58a58f7
Susana Ung <susana.ung@hotmail.com>
2020-12-05 17:43:58 -0500

    Added scoring function

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+const { defaultMaxListeners } = require("stream");
+
 let app = require("express")();
 let http = require("http").createServer(app);
 let io = require("socket.io")(http);
+
+// To change number to words for scoring
+let numberToWords = require("number-to-words");
+
+const { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } = require("constants");
 
 let connectedUsers = 0;
 
@@ -18,7 +25,7 @@ const createNewGame = (initialPlayer, scoreLimit = 10000) => {
     history: [], // The history of moves played so far in the game
     score: [0, 0], // Current (banked) score of both players
     scoreLimit, // The score limit for this game.
-    selectedDice: [] // The indices of currently selected (not cashed) dice.
+    selectedDice: [], // The indices of currently selected (not cashed) dice.
   };
 };
 
@@ -45,18 +52,109 @@ const rollDice = (previousDice) => {
   return dice;
 };
 
-// client -> dice [0,4,5]
+const checkDice = (dice) => {
+  // Debugging
+  console.log(dice); // Prints dice values and availabilities
 
-const getScoringOptions = (dice) => {
-  const options = [1, 1, 2, 3, 4, 5];
-  // 1: single one die: [0], 2: 2 ones die: [0,1], 3: single 5
+  var availableDice = [0, 0, 0, 0, 0, 0];
 
-  // dice[0].value = 1/2/3/4/5/6
-
-  // dice[0].available (true/false)
-
-  // return list of possible options
+  for (i = 0; i < dice.length; i++) {
+    if (dice[i].available == true) {
+      availableDice[dice[i].value - 1]++;
+    }
+  }
+  return availableDice;
 };
+
+const getScoringOptions = (availableDice) => {
+  var scoringOptions = [];
+
+  // Special rolls
+  if (availableDice.length == 6) {
+    // One to six (req 6 dice)
+    for (i = 0; i < 6; i++) {
+      // Breaks out of statement if number of dice for any value isn't 1
+      if (availableDice[i] != 1) {
+        break;
+      }
+      if (i == 5 && availableDice[i] == 1) {
+        scoringOptions.push("One to six*");
+      }
+    }
+
+    // Any three pairs (req 6 dice)
+    var pairCount = 0;
+    for (i = 0; i < 6; i++) {
+      // Checks for pairs and increments the counter
+      if (availableDice[i] == 2) {
+        pairCount++;
+      }
+    }
+    if (pairCount == 3) {
+      scoringOptions.push("Any three pairs*");
+    }
+  }
+
+  // Three of a kind or more
+  for (i = 1; i < 6; i++) {
+    // Starts at 2
+    if (availableDice[i] >= 3) {
+      // While the number of available dice is still above 3, add all valid combinations
+      for (h = availableDice[i]; h >= 3; h--) {
+        if (i != 5) {
+          scoringOptions.push(`${h} ${numberToWords.toWords(i + 1)}s`);
+        } else {
+          scoringOptions.push(`${h} ${numberToWords.toWords(i + 1)}es`); // Sixes
+        }
+      }
+    }
+
+    // Ones and Fives
+    switch (availableDice[0]) {
+      default:
+        // While the number of available dice is still above 0, add all valid combinations
+        for (i = availableDice[0]; i > 0; i--) {
+          if (i == 1) {
+            scoringOptions.push("Single one");
+            continue;
+          }
+          scoringOptions.push(`${i} ones`);
+        }
+        break;
+    }
+    switch (availableDice[4]) {
+      default:
+        // While the number of available dice is still above 0, add all valid combinations
+        for (i = availableDice[4]; i > 0; i--) {
+          if (i == 1) {
+            scoringOptions.push("Single five");
+            continue;
+          }
+          scoringOptions.push(`${i} fives`);
+        }
+        break;
+    }
+
+    // Checks for empty array
+    if (scoringOptions.length == 0) {
+      if (availableDice.length == 6) {
+        scoringOptions.push("No scoring dice"); // Combo if all dice don't give a combo
+      } else {
+        scoringOptions.push("Zilch!"); // Otherwise, zilch out
+      }
+    }
+
+    // debugging
+    console.log(scoringOptions);
+
+    // Return list of possible options
+    return scoringOptions;
+  }
+};
+
+// Prints dice values and all possible combinations
+getScoringOptions(checkDice(rollDice()));
+
 /*
 values = [0,0,0,0,0,0];
 values[0]++ // Found a 1
