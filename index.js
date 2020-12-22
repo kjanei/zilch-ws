62feab29e1777158f4e8532f6be2b71b1b5d9f74
Susana Ung <susana.ung@hotmail.com>
2020-12-22 01:36:17 -0500

    Refactored variables and functions

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ let path = require("path");
 let app = require("express")();
 let http = require("http").createServer(app);
 let io = require("socket.io")(http);
+let numberToWords = require("number-to-words");
 
 // https://stackoverflow.com/a/36041093
 // Express Middleware for serving static files
@@ -14,13 +15,11 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
-// To change number to words for scoring
-let numberToWords = require("number-to-words");
-
 let connectedUsers = 0;
 
 let gameState = null;
 let turnScore = 0;
+const NUMBER_OF_DICE = 6;
 
 // Cheat sheet for socket.io event emission:
 // https://socket.io/docs/v3/emit-cheatsheet/index.html
@@ -49,7 +48,7 @@ io.on("connection", (socket) => {
 
     let unscoredDice = [];
 
-    for (let i = 0; i < 6; i++) {
+    for (let i = 0; i < NUMBER_OF_DICE; i++) {
       if (gameState.dice[i].available && selectedDice[i]) {
         gameState.dice[i].available = false;
         unscoredDice.push(gameState.dice[i].value);
@@ -61,40 +60,39 @@ io.on("connection", (socket) => {
     io.emit("gameStateUpdate", gameState);
   });
 
-  // const getScoringOptions = (availableDice) => {
   socket.on("getScoringOptions", (chosenDice) => {
-    const availableDice = checkDice(chosenDice);
-    var scoringOptions = [];
-    // Special rolls
-    if (availableDice.length == 6) {
+    const scoringDiceArray = checkDice(chosenDice);
+    let scoringOptions = [];
+
+    if (scoringDiceArray.length === NUMBER_OF_DICE) {
       // One to six (req 6 dice)
-      for (i = 0; i < 6; i++) {
-        // Breaks out of statement if number of dice for any value isn't 1
-        if (availableDice[i] != 1) {
+      for (i = 0; i < NUMBER_OF_DICE; i++) {
+        if (scoringDiceArray[i] != 1) {
           break;
         }
-        if (i == 5 && availableDice[i] == 1) {
+        if (i === 5 && scoringDiceArray[i] === 1) {
           scoringOptions.push({ roll: "One to six*", score: 1500 });
         }
       }
+
       // Any three pairs (req 6 dice)
-      var pairCount = 0;
-      for (i = 0; i < 6; i++) {
-        // Checks for pairs and increments the counter
-        if (availableDice[i] == 2) {
+      let pairCount = 0;
+      for (i = 0; i < NUMBER_OF_DICE; i++) {
+        if (scoringDiceArray[i] === 2) {
           pairCount++;
         }
       }
-      if (pairCount == 3) {
+      if (pairCount === 3) {
         scoringOptions.push({ roll: "Any three pairs*", score: 1500 });
       }
     }
+
     // Three of a kind or more
-    for (i = 1; i < 6; i++) {
-      // Starts at 2
-      let h = availableDice[i];
+    for (i = 1; i < NUMBER_OF_DICE; i++) {
+      const h = scoringDiceArray[i];
       if (h >= 3) {
         if (i != 5 || i != 4) {
+          // Twos, threes, fours
           scoringOptions.push({
             roll: `${h} ${numberToWords.toWords(i + 1)}s`,
             score: i * 100 * (h - 2),
@@ -108,8 +106,9 @@ io.on("connection", (socket) => {
         }
       }
     }
+
     // Ones and Fives
-    switch ((i = availableDice[0])) {
+    switch ((i = scoringDiceArray[0])) {
       case 0:
         break;
       case 1:
@@ -126,7 +125,7 @@ io.on("connection", (socket) => {
         break;
     }
 
-    switch ((i = availableDice[4])) {
+    switch ((i = scoringDiceArray[4])) {
       case 0:
         break;
       case 1:
@@ -142,24 +141,23 @@ io.on("connection", (socket) => {
         });
         break;
     }
-    // Checks for empty array
-    if (scoringOptions.length == 0) {
+
+    if (scoringOptions.length === 0) {
       let chosenDiceLength = 0;
       let unavailableDice = 0;
-      for (let i = 0; i < 6; i++) {
-        chosenDiceLength += availableDice[i];
+      for (let i = 0; i < NUMBER_OF_DICE; i++) {
+        chosenDiceLength += scoringDiceArray[i];
         if (!gameState.dice[i].available) {
           unavailableDice++;
         }
       }
-      if (chosenDiceLength == 6) {
-        scoringOptions.push({ roll: "No scoring dice", score: 500 }); // Combo if all dice don't give a combo
-      } else if (chosenDiceLength + unavailableDice == 6) {
-        // after three consecutive zilch counts, lose 500 points ***
+      if (chosenDiceLength === NUMBER_OF_DICE) {
+        scoringOptions.push({ roll: "No scoring dice", score: 500 });
+      } else if (chosenDiceLength + unavailableDice === 6) {
+        // After three consecutive zilch counts, lose 500 points ***
         scoringOptions.push("Zilch!"); // Otherwise, zilch out
         socket.emit("enableZilch");
         gameState.consecutiveZilchCounter[gameState.currentPlayer - 1]++;
-        console.log(gameState.consecutiveZilchCounter);
       } else {
         scoringOptions.push("Choose some dice to see your options");
       }
@@ -171,26 +169,18 @@ io.on("connection", (socket) => {
     }
     enableCashIn(turnScore, potentialRollScore);
 
-    // Changes status to list of possible options
     io.emit("scoringOptions", JSON.stringify(scoringOptions));
   });
 });
 
-const enableCashIn = (turnScore, potentialRollScore) => {
-  let totalPotentialScore = turnScore + potentialRollScore;
-  if (totalPotentialScore >= 300) {
-    io.emit("enableCashIn");
-  }
-};
-
-http.listen(3000, () => {
-  console.log("Listening on port 3000");
-});
-
-const createNewGame = (initialPlayer, scoreLimit = 10000) => {
+const createNewGame = (
+  initialPlayer,
+  scoreLimit = 10000,
+  numberOfPlayers = 2
+) => {
   const currentPlayer = initialPlayer
     ? initialPlayer
-    : Math.floor(Math.random() * 2) + 1;
+    : Math.floor(Math.random() * numberOfPlayers) + 1;
 
   return {
     accumulatedPoints: 0, // The points accumulated, but not cashed in, on a specific turn.
@@ -198,9 +188,17 @@ const createNewGame = (initialPlayer, scoreLimit = 10000) => {
     dice: rollDice(),
     history: [], // The history of moves played so far in the game
     score: [0, 0], // Current (banked) score of both players
-    consecutiveZilchCounter: [0, 0], // Current number of consecutive zilches for both players
+    consecutiveZilchCounter: [0, 0],
     scoreLimit, // The score limit for this game.
+    numberOfPlayers,
   };
+};
+
+const enableCashIn = (turnScore, potentialRollScore) => {
+  const totalPotentialScore = turnScore + potentialRollScore;
+  if (totalPotentialScore >= 300) {
+    io.emit("enableCashIn");
+  }
 };
 
 const selectDice = (selection, previousDice) => {
@@ -223,7 +221,7 @@ const rollDice = (previousDice) => {
       if (die.available) {
         // If the die is available to be rolled, reroll its value.
         return {
-          value: Math.floor(Math.random() * 6) + 1,
+          value: Math.floor(Math.random() * NUMBER_OF_DICE) + 1,
           available: true,
         };
       }
@@ -233,35 +231,24 @@ const rollDice = (previousDice) => {
 
   // If no previous dice are supplied, return a fresh roll of 6 dice.
   const dice = [];
-  for (let i = 0; i < 6; i++) {
-    dice.push({ value: Math.floor(Math.random() * 6) + 1, available: true });
+  for (let i = 0; i < NUMBER_OF_DICE; i++) {
+    dice.push({
+      value: Math.floor(Math.random() * NUMBER_OF_DICE) + 1,
+      available: true,
+    });
   }
   return dice;
 };
 
+// Returns an array of ordered dice values for scoring
 const checkDice = (dice) => {
-  var availableDice = [0, 0, 0, 0, 0, 0];
-
+  let scoringDiceArray = [0, 0, 0, 0, 0, 0];
   for (i = 0; i < dice.length; i++) {
-    // if (dice[i].available != true) {
-    availableDice[dice[i].value - 1]++;
-    // }
+    scoringDiceArray[dice[i].value - 1]++;
   }
-  return availableDice;
+  return scoringDiceArray;
 };
 
-/*
-values = [0,0,0,0,0,0];
-values[0]++ // Found a 1
-values[1]++ // Found a 2
-...
-values[5]++ // Found a 6
-
-if there are any ones or fives this counts as a scoring "combo" ("single one, 2 ones, 2 fives, single five")
-if any of the values are >=3 that is a scoring combo ("3 ones, 4 twos, 5 threes, etc")
-if there are 6 dice:
-  if there are 3 values that are equal to 2 => three pairs
-  if all values are equal to 1 => 1-2-3-4-5-6
-else if nothing scored:
-  give the "nothing" combo
-*/
+http.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
